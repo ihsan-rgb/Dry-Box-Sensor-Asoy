351a0a846770bcbf3beb8d549745f9c84dadbd1c
ihsan-rgb <16720255@mahasiswa.itb.ac.id>
2022-11-21 20:06:01 +0700

    First Launch

---
+++ src/main.cpp
@@ -0,0 +1,109 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <DHT.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define DHTPIN 14
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+DHT dht(DHTPIN, DHTTYPE);
+
+void displayOled() {
+
+  // Read humidity
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(false);
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(0.5);
+  display.setCursor(5, 8);
+  display.print("Humidity:");
+  display.setCursor(80, 8);
+  display.print(h);
+  display.print("%");
+  display.setCursor(5, 20);
+  display.print("Temperature:");
+  display.setCursor(80, 20);
+  display.print(t);
+  display.print((char)247); // degree symbol
+  display.print("C");
+  display.display();
+
+}
+
+void setup() {
+
+  Serial.begin(115200);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(33, OUTPUT);
+  pinMode(27, OUTPUT);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Don't proceed, loop forever
+  }
+  display.clearDisplay();
+  dht.begin();
+
+}
+
+void loop() {
+
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  bool t_status = (t >= 70);
+  bool h_status = (t >= 27);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+  displayOled();
+
+  digitalWrite(12, h_status);
+  digitalWrite(13, !h_status);
+  digitalWrite(27, t_status);
+  digitalWrite(33, !t_status);
+
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+}
