b40db44ffff0c36fdf6bc683e69adc40e3294c44
ihsan-rgb <16720255@mahasiswa.itb.ac.id>
2022-12-02 12:26:52 +0700

    Modifikasi Sermon

--- src/main.cpp
+++ src/main.cpp
@@ -20,38 +20,8 @@ DHT dht(DHTPIN, DHTTYPE);
 #define LED_3 17
 #define LED_4 16
 
-void displayOled() {
-
-  // Read humidity
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(false);
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-
-
-
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setTextSize(0.5);
-  display.setCursor(5, 8);
-  display.print("Humidity:");
-  display.setCursor(80, 8);
-  display.print(h);
-  display.print("%");
-  display.setCursor(5, 20);
-  display.print("Temperature:");
-  display.setCursor(80, 20);
-  display.print(t);
-  display.print((char)247); // degree symbol
-  display.print("C");
-  display.display();
-
-}
+float h = 0;
+float t = 0;
 
 void setup() {
 
@@ -82,23 +52,19 @@ void loop() {
 
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
+  h = dht.readHumidity();
   // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
+  t = dht.readTemperature();
   
   bool t_status = (t >= 70);
   bool h_status = (h >= 55);
 
   // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
+  if (isnan(h) || isnan(t)) {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
+
   displayOled();
 
   digitalWrite(LED_1, h_status);
@@ -106,10 +72,30 @@ void loop() {
   digitalWrite(LED_3, t_status);
   digitalWrite(LED_4, !t_status);
 
-
+  Serial.println();
   Serial.print(h);
   Serial.print(" ");
   Serial.print(t);
   Serial.print(" ");
+
+}
+
+void displayOled() {
+
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(0.5);
+  display.setCursor(5, 8);
+  display.print("Humidity:");
+  display.setCursor(80, 8);
+  display.print(h);
+  display.print("%");
+  display.setCursor(5, 20);
+  display.print("Temperature:");
+  display.setCursor(80, 20);
+  display.print(t);
+  display.print((char)247); // degree symbol
+  display.print("C");
+  display.display();
 
 }
